d007afb133495f89fc9565919902b11ab6e41fd9
fffkkkooo <enemy872@gmail.com>
2023-02-20 22:34:37 +0300

    N1

--- django_movie/movies/admin.py
+++ django_movie/movies/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category, Actor, Rating, RatingStar, Reviews, Genre, MovieShots, Movie
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Actor)
+admin.site.register(Rating)
+admin.site.register(RatingStar)
+admin.site.register(Reviews)
+admin.site.register(Genre)
+admin.site.register(MovieShots)
+admin.site.register(Movie)
